479338e6cbfd5e619a24f44c1b8673a38eb24c30
venecy <gu.lewei@qq.com>
2016-05-22 21:54:18 +0800

    测试

---
+++ solutions/test.js
@@ -0,0 +1,6 @@
+var tau = {
+    search: ["search ant 1", "search ant 2"],
+    detection: ["detection ant 1", "detection ant 2"]
+}
+var catagray = "search";
+console.log("search: " + tau[catagray]);
